9613893d99a351c40a041c62afb35ce426602aa9
liberbell <liberbell@gmail.com>
2018-10-18 08:32:48 +0900

    pyblocks3

--- pyblocks1.py
+++ pyblocks1.py
@@ -1,11 +1,21 @@
 x = 42
 y = 42
 
-if x > y:
-    print('x > y: x is {} and y is {}'.format(x,y))
-elif x < y:
-    print('x < y: x is {} and y is {}'.format(x,y))
-elif x == y:
-    print('x = y: x is {} and y is {}'.format(x,y))
+# if x > y:
+#     print('x > y: x is {} and y is {}'.format(x,y))
+# elif x < y:
+#     print('x < y: x is {} and y is {}'.format(x,y))
+# elif x == y:
+#     print('x = y: x is {} and y is {}'.format(x,y))
+if x == 5:
+    print('do five stuff')
+elif x == 6:
+    print('do six stuff')
+elif x == 7:
+    print('do seven stuff')
+elif x == 8:
+    print('do eight stuff')
+elif x == 42:
+    print('do forty-two stuff')
 else:
     print('do something else')
