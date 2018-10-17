568e399c641dae121c0f2dae14e615f43e9e6ac6
liberbell <liberbell@gmail.com>
2018-10-17 09:17:37 +0900

    pyblocks4

--- pyblocks1.py
+++ pyblocks1.py
@@ -1,5 +1,9 @@
 x = 42
 y = 73
 
-if x < y:
-    print('x < y: x is {} and y is {}'.format(x,y))
+if x > y:
+    print('x > y: x is {} and y is {}'.format(x,y))
+elif x < y:
+    print('x < y: x is {} and y is {}'.format(x,y))    
+else:
+    print('do something else')
