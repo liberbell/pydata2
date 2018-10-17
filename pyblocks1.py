9d2ec96665954dc9507bcecdff84e00d8db0f587
liberbell <liberbell@gmail.com>
2018-10-17 09:09:37 +0900

    pyblocks2

--- pyblocks1.py
+++ pyblocks1.py
@@ -2,10 +2,4 @@ x = 42
 y = 73
 
 if x < y:
-    z = 112
     print('x < y: x is {} and y is {}'.format(x,y))
-    print('line2')
-    print('line3')
-    print('line4')
-
-print('z is {}'.format(z))
