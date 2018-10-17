7b5f3044c51489902c6ce709e7ed4f9732edac56
liberbell <liberbell@gmail.com>
2018-10-18 08:30:06 +0900

    pyblocks2

--- pyblocks1.py
+++ pyblocks1.py
@@ -1,11 +1,11 @@
 x = 42
-y = 73
+y = 42
 
 if x > y:
     print('x > y: x is {} and y is {}'.format(x,y))
 elif x < y:
     print('x < y: x is {} and y is {}'.format(x,y))
 elif x == y:
-    print('x == y: x is {} and y is {}'.format(x,y))    
+    print('x = y: x is {} and y is {}'.format(x,y))
 else:
     print('do something else')
