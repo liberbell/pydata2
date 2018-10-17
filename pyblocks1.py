e7cc2fd20edf51351c2d6090c12ead0150227357
liberbell <liberbell@gmail.com>
2018-10-17 09:09:10 +0900

    pyblocks1

---
+++ pyblocks1.py
@@ -0,0 +1,11 @@
+x = 42
+y = 73
+
+if x < y:
+    z = 112
+    print('x < y: x is {} and y is {}'.format(x,y))
+    print('line2')
+    print('line3')
+    print('line4')
+
+print('z is {}'.format(z))
