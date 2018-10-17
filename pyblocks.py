2cc4a209abc7275a305132fcd2f0f451bca953d6
liberbell <liberbell@gmail.com>
2018-10-17 09:07:04 +0900

    pyblocks3

--- pyblocks.py
+++ pyblocks.py
@@ -1,10 +1,11 @@
 x = 42
 y = 73
 
-if x > y:
+if x < y:
+    z = 112
     print('x < y: x is {} and y is {}'.format(x,y))
     print('line2')
     print('line3')
     print('line4')
 
-print('something else')
+print('z is {}'.format(z))
