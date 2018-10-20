3bc902e721d91a1a271f3b309a9cfaeb5e55d9d0
liberbell <liberbell@gmail.com>
2018-10-20 09:21:44 +0900

    ftypes4

--- ftypes.py
+++ ftypes.py
@@ -1,7 +1,15 @@
+from decimal import *
+
 x = 7 * 3.14159
 y = .1 + .1 + .1 - .3
 
+a = Decimal('.10')
+b = Decimal('.30')
+c = a + a + a - b
+
 print('x is {}'.format(x))
 print('y is {}'.format(y))
+print('z is {}'.format(c))
 print(type(x))
 print(type(y))
+print(type(c))
