da55f8b766cff6d0d5bbaa7c4a3514403660b903
liberbell <liberbell@gmail.com>
2018-10-21 12:46:03 +0900

    sequence4

--- sequence.py
+++ sequence.py
@@ -4,5 +4,5 @@ y = { 'one':1, 'two':2, 'three':3, 'four':4, 'five':5}
 for i in x:
     print('i is {}'.format(i))
 
-for h in y:
-    print('h is {}'.format(h))
+for h, j in y.items():
+    print('h: {},j: {}'.format(h, j))
