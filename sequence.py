c8a7966c85d50f4869d4970a4986184a80aa6b5c
liberbell <liberbell@gmail.com>
2018-10-21 12:43:32 +0900

    sequence3

--- sequence.py
+++ sequence.py
@@ -1,5 +1,8 @@
 x = range(5)
+y = { 'one':1, 'two':2, 'three':3, 'four':4, 'five':5}
 
 for i in x:
     print('i is {}'.format(i))
-    print(type(i))
+
+for h in y:
+    print('h is {}'.format(h))
