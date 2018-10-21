8869a00d7e3ff23b97e00d5e0a81e2889cd9f42e
liberbell <liberbell@gmail.com>
2018-10-21 12:56:37 +0900

    nsequence4

--- nsequence.py
+++ nsequence.py
@@ -1,6 +1,11 @@
 x = (1, 'two', 3.0, [4, 'four'], 5)
-y = (1, 'two', 3.0, [4, 'four'], 5)
+y = (2, 'two', 3.0, [4, 'four'], 5)
 
 print('x is {}'.format(x))
 print(id(x[0]))
 print(id(y[0]))
+
+if x[0] is y[0]:
+    print('yep')
+else:
+    print('nope')
