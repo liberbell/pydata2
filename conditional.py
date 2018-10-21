68805bd886820bea8d96720c810ef146812b5162
liberbell <liberbell@gmail.com>
2018-10-21 13:09:51 +0900

    conditional3

--- conditional.py
+++ conditional.py
@@ -1,7 +1,16 @@
+x = 5
 
-if True:
-    print('if true')
-elif False:
-    print('if false')
+if x == 0:
+    print('zero true')
+elif x == 1:
+    print('one true')
+elif x == 2:
+    print('two true')
+elif x == 3:
+    print('three true')
+elif x == 4:
+    print('four true')
+elif x == 5:
+    print('five True')
 else:
     print('neither true')
