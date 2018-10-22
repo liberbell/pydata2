590e25455468160dd68469055fdb9113d6da19b9
liberbell <liberbell@gmail.com>
2018-10-22 09:04:11 +0900

    bitwise3

--- bitwise.py
+++ bitwise.py
@@ -2,5 +2,5 @@ x = 0x0a
 y = 0x02
 z = x & y
 
-print(f'(hex) x is {x:02x}, y is {y:02x}, z is {z:02x}')
+print(f'(hex) x is {x:04x}, y is {y:04x}, z is {z:04x}')
 print(f'(bin) x is {x:08b}, y is {y:08b}, z is {z:08b}')
