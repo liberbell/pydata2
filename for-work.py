fe08fa0cbcd1ea0cad837e2441038fe7d1b80c83
liberbell <liberbell@gmail.com>
2018-10-23 09:20:48 +0900

    for6

--- for-work.py
+++ for-work.py
@@ -2,5 +2,7 @@ animals = ('beer', 'bunny', 'dog', 'cat', 'velociraptor')
 
 for pet in animals:
     if pet == 'dog': continue
-    if pet == 'velociraptor': break
+    # if pet == 'velociraptor': break
     print(pet)
+else:
+    print('that is all the animals')
