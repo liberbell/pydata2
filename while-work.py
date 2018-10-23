5e0558c5b9f02bf20fb004efad32936287801433
liberbell <liberbell@gmail.com>
2018-10-23 09:10:33 +0900

    while6

--- while-work.py
+++ while-work.py
@@ -9,3 +9,7 @@ while pw != secret:
     count += 1
     if count > max_attempt: break
     pw = input(f'{count}: what`s the secret word?')
+else:
+    auth = True
+
+print('Authorized' if auth else 'Calling the FBI')
