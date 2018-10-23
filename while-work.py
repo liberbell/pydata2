d696c2792fd230b5e9b0774bc73dc668aa1d74be
liberbell <liberbell@gmail.com>
2018-10-23 09:06:54 +0900

    while4

--- while-work.py
+++ while-work.py
@@ -1,5 +1,11 @@
 secret = 'swordfish'
 pw = ''
+auth = False
+count = 0
+max_attempt = 5
 
 while pw != secret:
-    pw = input('what`s the secret word?')
+    # pw = input('what`s the secret word?')
+    if count > max_attempt: break
+    pw = input(f'{count}: what`s the secret word?')
+    count += 1
