6a6c4e35b34cb65992f5ab94b0fbbc8d0027d9a8
liberbell <liberbell@gmail.com>
2018-10-26 16:37:52 +0900

    kwargs2

--- kwargs.py
+++ kwargs.py
@@ -4,4 +4,8 @@ def main():
 def kitten(**kwargs):
     if len(kwargs):
         for k in kwargs:
-            print('kitten {} says ')
+            print('kitten {} says {}'.format(k, kwargs[k]))
+        else:
+            print('meow.')
+
+if __name__ == '__main__': main()
