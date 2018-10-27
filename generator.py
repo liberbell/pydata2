11fea35e6984302ec9ee1545973400671d4149d3
liberbell <liberbell@gmail.com>
2018-10-27 16:53:50 +0900

    generator6

--- generator.py
+++ generator.py
@@ -16,7 +16,7 @@ def inclusive_range(*args):
         (start, stop) = args
     elif numargs == 3:
         (start, stop, step) = args
-    else: rasie TypeError(f'expected at most 3 argement, got {numargs}')
+    else: raise TypeError(f'expected at most 3 argement, got {numargs}')
     
     i = start
     while i <= stop:
