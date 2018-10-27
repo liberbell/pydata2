57cc3d9c8c44a63e063a986011eb09ae499c8d51
liberbell <liberbell@gmail.com>
2018-10-27 17:05:19 +0900

    generator6

--- generator 2.py
+++ generator 2.py
@@ -1,28 +1,28 @@
-
-
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
 
 def main():
-    for i in inclusive_range(25):
+    for i in inclusive_range(5, 25, 5):
         print(i, end = ' ')
     print()
 
 def inclusive_range(*args):
     numargs = len(args)
     start = 0
-    stop = 1
+    step = 1
 
-    
+    # initialize parameters
     if numargs < 1:
-        raise TypeError(f'expected at lease 1 argement, got {numargs}')
+        raise TypeError(f'expected at least 1 argument, got {numargs}')
     elif numargs == 1:
         stop = args[0]
     elif numargs == 2:
         (start, stop) = args
     elif numargs == 3:
         (start, stop, step) = args
-    else: raise TypeError(f'expected at most 3 argement, got {numargs}')
-    
+    else: raise TypeError(f'expected at most 3 arguments, got {numargs}')
 
+    # generator
     i = start
     while i <= stop:
         yield i
