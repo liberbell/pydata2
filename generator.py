b257629850807a17ace93dcd2e02bddaa769cf2b
liberbell <liberbell@gmail.com>
2018-10-27 16:42:15 +0900

    generator1

--- generator.py
+++ generator.py
@@ -2,3 +2,13 @@ def main():
     for i in inclusive_range(25):
         print(i, end = ' ')
     print()
+
+def inclusive_range(*args):
+    numargs = len(args)
+    start = 0
+    stop = 1
+
+    if numargs < 1:
+        raise TypeError(f'expected at lease 1 argement, got {numargs}')
+    elif numargs == 1:
+        
