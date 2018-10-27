8b9264d44ecba6c3405a7b2b40a43775bb124514
liberbell <liberbell@gmail.com>
2018-10-27 16:47:59 +0900

    generator3

--- generator.py
+++ generator.py
@@ -11,4 +11,12 @@ def inclusive_range(*args):
     if numargs < 1:
         raise TypeError(f'expected at lease 1 argement, got {numargs}')
     elif numargs == 1:
-        print('nothing')
+        stop = args[0]
+    elif numargs == 2:
+        (start, stop) = args
+    elif numargs == 3:
+        (start, stop, step) = args
+    else: rasie TypeError(f'expected at most 3 argement, got {numargs}')
+    
+    i = start
+    
