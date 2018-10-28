8ecdf324e3989df72019d614cac7bc574628d7ec
liberbell <liberbell@gmail.com>
2018-10-28 12:25:26 +0900

    hello2

--- hello2.py
+++ hello2.py
@@ -3,10 +3,14 @@
 def f1(f):
     def f2():
         print('this is before function call')
+        f()
+        print('this is after function call')
     return f2
 
 def f3():
     print('this is f3')
 
-x = f1()
-x()
+# x = f1()
+# x()
+x = f1(f3)
+(x)
