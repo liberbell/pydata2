c2ade49caa0efd2977f751a2c7b9285e4709afb1
liberbell <liberbell@gmail.com>
2018-10-28 12:46:47 +0900

    list2

--- list.py
+++ list.py
@@ -3,7 +3,8 @@ def main():
     print_list(game)
 
 def print_list(o):
-    for i in print(i, end=' ', flush=True)
+    for i in o:
+        print(i, end=' ', flush=True)
     print()
 
 if __name__ == '__main__' : main()
