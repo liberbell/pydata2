f41b3a428f5b698ca1ad3569de94e2d28216d0b5
liberbell <liberbell@gmail.com>
2018-10-29 07:41:15 +0900

    dict4

--- dictonary.py
+++ dictonary.py
@@ -3,8 +3,8 @@ def main():
         'giraffe': 'I am a giraffe!', 'dragon': 'rawr'}
     # for k, v in animals.items():
     #     print(f'{k}: {v}')
-    for k in animals.keys():
-        print(k)
+    for v in animals.values():
+        print(v)
     # print_dict(animals)
 
 def print_dict(o):
