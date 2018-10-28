f9ce398d2a4365d56f9d827260bbb2950a5f77ee
liberbell <liberbell@gmail.com>
2018-10-28 12:45:13 +0900

    list1

---
+++ list.py
@@ -0,0 +1,9 @@
+def main():
+    game = ['rock', 'paper', 'scissor', 'lizard', 'spock']
+    print_list(game)
+
+def print_list(o):
+    for i in print(i, end=' ', flush=True)
+    print()
+
+if __name__ == '__main__' : main()
