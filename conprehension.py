ea53a95670bccd0b09efa92c9e3841012074ad31
liberbell <liberbell@gmail.com>
2018-10-30 07:47:40 +0900

    conprehension6

--- conprehension.py
+++ conprehension.py
@@ -1,7 +1,9 @@
 def main():
-    seq = range(11)
+    seq = range(100)
     # seq2 = [x for x in seq if x % 3 != 0]
-    seq2 = [(x, x**2) for x in seq]
+    from math import pi
+    # seq2 = [(x, x**2) for x in seq]
+    seq2 = [round(pi, i) for i in seq]
     print_list(seq)
     print_list(seq2)
 
