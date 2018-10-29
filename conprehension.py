f1d0cc6bbbe44b1d61714201bf6015ad4030e949
liberbell <liberbell@gmail.com>
2018-10-30 07:48:50 +0900

    conprehension7

--- conprehension.py
+++ conprehension.py
@@ -3,7 +3,8 @@ def main():
     # seq2 = [x for x in seq if x % 3 != 0]
     from math import pi
     # seq2 = [(x, x**2) for x in seq]
-    seq2 = [round(pi, i) for i in seq]
+    # seq2 = [round(pi, i) for i in seq]
+    seq2 = { x: x**2 for x in seq}
     print_list(seq)
     print_list(seq2)
 
