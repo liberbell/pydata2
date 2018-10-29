7ec41683838376c5b398441d6ba7a4993ca92e3d
liberbell <liberbell@gmail.com>
2018-10-30 07:51:37 +0900

    conprehension9

--- conprehension.py
+++ conprehension.py
@@ -1,12 +1,12 @@
 def main():
-    seq = range(100)
+    seq = range(11)
     # seq2 = [x for x in seq if x % 3 != 0]
     from math import pi
     # seq2 = [(x, x**2) for x in seq]
     # seq2 = [round(pi, i) for i in seq]
-    seq2 = { x: x**2 for x in seq}
+    seq2 = {x for x in 'superduper' if x not in 'pd'}
     print_list(seq)
-    print(seq2)
+    print_list(seq2)
 
 def print_list(o):
     for x in o:
