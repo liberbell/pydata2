ff675a4025b328b2c462426c5d6e73d0b69977f0
liberbell <liberbell@gmail.com>
2018-10-31 08:40:43 +0900

    mixed1

--- mixed2.py
+++ mixed2.py
@@ -8,3 +8,13 @@ def main():
     d = dict(one = r, two = l, three = s)
     mixed = [l, r, s, d, t]
     disp(mixed)
+
+def disp(o):
+    global dlevel
+    
+    dlevel += 1
+    if isinstance(o, list): print_list(o)
+    elif isinstance(o, range): print_list(o)
+    elif isinstance(o, tuple): print_tuple(o)
+    elif isinstance(o, set): print_set(o)
+    elif isinstance(o, dict): print_dict(o)
