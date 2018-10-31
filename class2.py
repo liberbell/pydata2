192e5d54759ce4f7d593f2e0d405930de2f3ea9f
liberbell <liberbell@gmail.com>
2018-10-31 09:10:04 +0900

    class1

---
+++ class2.py
@@ -0,0 +1,11 @@
+class duck:
+    sound = 'Quack Quack.'
+    movement = 'Walks like a duck.'
+
+    def quack(self):
+        print(self.sound)
+
+    def movement(self):
+        print(self.movement)
+
+def 
