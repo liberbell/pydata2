53a189638c48a0f9811bced86789b472d44ca5e0
liberbell <liberbell@gmail.com>
2018-10-31 09:18:06 +0900

    constructor1

---
+++ constructor.py
@@ -0,0 +1,14 @@
+class animal:
+    def __init__(self, type, name, sound):
+        self._type = type
+        self._name = name
+        self._sound = sound
+
+        def type(self):
+            return self._type
+
+        def name(self):
+            return self._name
+
+        def sound(self):
+            return self._sound
