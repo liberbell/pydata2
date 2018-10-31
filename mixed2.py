1c852b1dcc997eb3a74918e2f997884551b5c72d
liberbell <liberbell@gmail.com>
2018-10-31 09:04:37 +0900

    mixed6

--- mixed2.py
+++ mixed2.py
@@ -13,17 +13,18 @@ def disp(o):
     global dlevel
     
     dlevel += 1
-    if isinstance(o, list): print_list(o)
+    if   isinstance(o, list):  print_list(o)
     elif isinstance(o, range): print_list(o)
     elif isinstance(o, tuple): print_tuple(o)
-    elif isinstance(o, set): print_set(o)
-    elif isinstance(o, dict): print_dict(o)
-    elif o is None: print('Nada', end='x', flush=True)
+    elif isinstance(o, set):   print_set(o)
+    elif isinstance(o, dict):  print_dict(o)
+    elif o is None: print('Nada', end=' ', flush=True)
     else: print(repr(o), end=' ', flush=True)
     dlevel -= 1
 
     if dlevel <= 1:
         print()
+        
 def print_list(o):
     print('[', end=' ')
     for x in o:
