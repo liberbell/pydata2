81a51f1c4dbd6b44cc79bbf58019b254dd724cd4
liberbell <liberbell@gmail.com>
2018-11-02 08:49:38 +0900

    string3

--- string.py
+++ string.py
@@ -28,6 +28,18 @@ class Animal:
     def __str__(self):
         return f'The {self.type()} is named "{self.name()}" and says "{self.sound()}".'
 
+class Duck(Animal):
+    def __init__(self, **kwargs):
+        self._type = 'duck'
+        if 'type' in kwargs:
+            del kwargs['type']
+        super().__init__(**kwargs)
+
+class Kitten(Animal):
+    def __init__(self, **kwargs):
+        self._type = 'kitten'
+        if 'type' in kwargs:
+            del kwargs['type']
 def print_animal(o):
     if not isinstance(o, Animal):
         raise TypeError('print_animal(): requires an Animal')
