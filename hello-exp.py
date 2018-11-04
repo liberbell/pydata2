cd1fa05379d738823aa2098b40b29ee1855025fd
liberbell <liberbell@gmail.com>
2018-11-05 07:59:11 +0900

    except4

--- hello-exp.py
+++ hello-exp.py
@@ -1,10 +1,12 @@
+import sys
+
 def main():
     try:
-        x = 5/3
+        x = 5/0
     except ValueError:
         print('I caught a ValueError {}'.format)
-    except ZeroDivisionError:
-        print('dont divide by zero')
+    except:
+        print(f'unknown error: {sys.exc_info()}')
     else:
         print('good job')
         print(x)
