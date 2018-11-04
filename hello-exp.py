0943e8530b7f842b27e75c7c1608d93e7d1cf8e5
liberbell <liberbell@gmail.com>
2018-11-05 07:56:34 +0900

    except3

--- hello-exp.py
+++ hello-exp.py
@@ -1,10 +1,13 @@
 def main():
     try:
-        x = 5/0
+        x = 5/3
     except ValueError:
         print('I caught a ValueError {}'.format)
     except ZeroDivisionError:
         print('dont divide by zero')
+    else:
+        print('good job')
+        print(x)
 
 if __name__ == '__main__':
     main()
