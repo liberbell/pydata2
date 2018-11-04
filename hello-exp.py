3dd2dfaa06a888aad041e70daca1828eeca0f631
liberbell <liberbell@gmail.com>
2018-11-04 11:31:49 +0900

    hello4

--- hello-exp.py
+++ hello-exp.py
@@ -2,7 +2,7 @@ def main():
     try:
         x = int('foo')
     except ValueError:
-        print('I caught a ValueError')
+        print('I caught a ValueError {}'.format)
 
 if __name__ == '__main__':
     main()
