13112b4e0b6e528ee7be758a4c6f85f123a967be
liberbell <liberbell@gmail.com>
2018-11-09 09:23:04 +0900

    copybin2

--- copybin.py
+++ copybin.py
@@ -1,3 +1,10 @@
 def main():
     infile = open('berlin.jpg', 'rb')
     outfile = open('berlin-copy.jpg', 'wb')
+    while True:
+        buf = infile.read(10240)
+        if buf:
+            outfile.write(buf)
+            print('.', end='', flush=True)
+        else:
+            break
