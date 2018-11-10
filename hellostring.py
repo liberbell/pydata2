4552a476298c659a07bd4d41d15b2b65266d8312
liberbell <liberbell@gmail.com>
2018-11-10 12:21:01 +0900

    hellostring3

--- hellostring.py
+++ hellostring.py
@@ -1,7 +1,7 @@
 class bunny:
     def __init__(self, n):
         self._n = n
-    def __rper__(self):
+    def __repr__(self):
         return f'the number of bunnies is {self._n}'
 
 s = bunny(47)
