4e6ca3d9acf43786f72b470e4af559dbf8b02cdc
liberbell <liberbell@gmail.com>
2018-11-12 08:41:34 +0900

    hellocontainer14

--- hellocontainer.py
+++ hellocontainer.py
@@ -1,12 +1,16 @@
-x = (1, 2, 3, 4, 5)
-y = (6, 7, 8, 9, 10)
+# x = (1, 2, 3, 4, 5)
+# y = (6, 7, 8, 9, 10)
+x = ('cat', 'dog', 'rabbit', 'velociraptor')
+
+for i, v in enumerate(x):
+    print(f'{i}, {v}')
 
 # for i in y:
 #     print(i)
-z = zip(x, y)
-print(x)
-print(y)
-print(z)
-
-for a, b in z:
-    print(f'{a} - {b}')
+# z = zip(x, y)
+# print(x)
+# print(y)
+# print(z)
+#
+# for a, b in z:
+#     print(f'{a} - {b}')
