afd08a5812c2899c175cb19303ab1cc43e6d4b2d
liberbell <liberbell@gmail.com>
2018-11-12 08:38:23 +0900

    hellocontainer13

--- hellocontainer.py
+++ hellocontainer.py
@@ -1,8 +1,12 @@
-x = (1, 1, 1)
-y = all(x)
+x = (1, 2, 3, 4, 5)
+y = (6, 7, 8, 9, 10)
 
 # for i in y:
 #     print(i)
-
+z = zip(x, y)
 print(x)
 print(y)
+print(z)
+
+for a, b in z:
+    print(f'{a} - {b}')
