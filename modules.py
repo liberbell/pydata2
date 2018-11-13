cac51c7ce34b496aaaccc479c8f35150884786da
liberbell <liberbell@gmail.com>
2018-11-13 09:17:05 +0900

    modules10

--- modules.py
+++ modules.py
@@ -1,12 +1,15 @@
 import sys
 import os
 import random
+import datetime
 
 def main():
     v = list(range(25))
     print(v)
     random.shuffle(v)
     print(v)
+    now = datetime.datetime.now()
+    print(now)
     # print('Python version is {}.{}.{}'.format(*v))
 
 if __name__ == '__main__':
