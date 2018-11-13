c118c488ae123254cec697ebd93b2a71786a47a0
liberbell <liberbell@gmail.com>
2018-11-14 08:46:57 +0900

    saytime2

--- saytime.py
+++ saytime.py
@@ -14,5 +14,16 @@ def main():
 
     print('\ntime test:')
     list = (
-        
+    (0, 0), (0, 1), (11, 0), (12, 0), (13, 0), (12, 29), (12, 30),
+    (12, 31), (12, 15), (12, 30), (12, 45), (11, 59), (23, 15),
+    (23, 59), (12, 59), (13, 59), (1, 60), (24, 0)
     )
+    for l in list:
+        st.time(*l)
+        print(st.digits(), st.words())
+
+    st.time_t() # set time to now
+    print('\nlocal time is ' + st.words())
+
+if __name__ == '__main__':
+    main()
