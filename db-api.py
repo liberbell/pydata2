500a1d52ac982e2dbe1da056e7ff0bc056766051
liberbell <liberbell@gmail.com>
2018-11-14 09:15:57 +0900

    db-api2

--- db-api.py
+++ db-api.py
@@ -4,3 +4,9 @@ def main():
     print('connect')
     db = sqlite3.connect('db-api.db')
     curl = db.cursor()
+    print('create')
+    cur.execute('DROP TABLE IF EXISTS test')
+    cur.execute('''
+        CREATE TABLE test (
+            id 
+        ))
